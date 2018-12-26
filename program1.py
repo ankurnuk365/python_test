2b3b9904fea9463e0f571bf921f9262e6022e827
Ankur Choraywal <ankur.choraywal@news.co.uk>
2018-12-26 17:38:41 +0530

    adds math operation code

---
+++ program1.py
@@ -0,0 +1,16 @@
+
+
+def add_2_numbers(x, y):
+	return x + y
+
+
+def subtract_2_numbers(x, y):
+	return x - y
+
+
+def divide_2_numbers(x, y):
+	return x / y
+
+
+def multiply_2_numbers(x, y):
+	return x * y
